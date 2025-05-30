65ce8b2399945998039e611ae7837237f0492d8e
powero4ek <124286600+powero4ek@users.noreply.github.com>
2025-05-30 09:35:09 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,51 +1,62 @@
-interface HighVolt{
-    public void print();
+interface socket380V{
+    void ChargingOn();
+    void ChargingOff();
 }
-interface LowVolt{
-    public void print();
-}
-class threeHundVolt implements HighVolt{
-    int voltage;
 
-    @Override
-    public void print() {
-        System.out.println("Напряжение 380 вольт");
+class Charger380 implements socket380V{
+    public void ChargingOn(){
+        System.out.println("Charger 380V is on");
+    }
+    public void ChargingOff(){
+        System.out.println("Charger 380V is off");
     }
 }
-class twoHundVolt implements LowVolt{
-    int voltage;
 
-    @Override
-    public void print() {
-        System.out.println("Напряжение 220 вольт");
+class Rozetka{
+    private socket380V socket380V;
+    public Rozetka(socket380V socket380V){
+        this.socket380V = socket380V;
+    }
+    public void Test(){
+        socket380V.ChargingOn();
+        socket380V.ChargingOff();
     }
 }
-class Charging{
-    private HighVolt highVolt;
-    public Charging(HighVolt highVolt){
-        this.highVolt=highVolt;
-    }
-    public void charge(){
-        highVolt.print();
-    }
-}
-class Adapter implements HighVolt{
-    LowVolt lowVolt;
-    public Adapter(LowVolt lowVolt){
-        this.lowVolt = lowVolt;
-    }
 
-    @Override
-    public void print() {
-        lowVolt.print();
+interface socket220V{
+    void ChargingOn220V();
+    void ChargingOff220V();
+}
+
+class Charger220 implements socket220V{
+    public void ChargingOn220V() {
+        System.out.println("Charger 220V is on");
+    }
+    public void ChargingOff220V() {
+        System.out.println("Charger 220V is off");
+    }
+}
+
+class SocketAdapter implements socket380V{
+    Charger220 charger220;
+    public SocketAdapter(Charger220 charger220){
+        this.charger220 = charger220;
+    }
+    public void ChargingOn(){
+        charger220.ChargingOn220V();
+    }
+    public void ChargingOff(){
+        charger220.ChargingOff220V();
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        HighVolt v380 = new threeHundVolt();
-        Adapter v220 = new Adapter(new twoHundVolt());
-        Charging charging = new Charging(v220);
-        charging.charge();
+        socket380V ch380v = new Charger380();
+        Rozetka rz = new Rozetka(ch380v);
+        rz.Test();
+        SocketAdapter ch220v = new SocketAdapter(new Charger220());
+        Rozetka rzAdapter = new Rozetka(ch220v);
+        rzAdapter.Test();
     }
 }
